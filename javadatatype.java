bcaa3dbd3f28f854304ea4c287f3de2739663058
Madhavkumaryadav <ymadhav081@gmail.com>
2024-10-15 21:57:59 +0530

    new series java

---
+++ javadatatype.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class javadatatype{
+    public static void main(String [] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter your first number : ");
+        int a=sc.nextInt();
+        System.out.print("Enter your second number : ");
+        int b=sc.nextInt();
+        
+        System.out.println(a + " + " + b +" = " +(a+b));
+        
+    }
+
+}
